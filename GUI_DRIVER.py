09f6d7b14a9a81ce5b8e825456752810c68e9d8b
MatthewMcNatt <matthew.mcnatt@mavs.uta.edu>
2023-10-25 09:51:55 -0500

    template to fill in

--- GUI_DRIVER.py
+++ GUI_DRIVER.py
@@ -1,1 +1,47 @@
 #GUI CODE
+import tkinter as tk
+from tkinter import ttk
+from NL_SQL_Engine import * 
+
+#main windows
+root = tk.Tk()
+root.geometry("800x400")
+root.title("SQL SCAFFOLD")
+
+# Create a custom style for the Notebook tabs
+style = ttk.Style()
+style.configure("TNotebook.Tab", padding=[30, 5])  # Adjust the padding here
+
+# Create a Notebook (Tabbed interface) with the custom style
+notebook = ttk.Notebook(root, style="TNotebook")
+
+# Set up frames and add them to the gui
+createSchemaFrame = ttk.Frame(notebook)
+assessSchemaFrame = ttk.Frame(notebook)
+generateQueryFrame = ttk.Frame(notebook)
+
+notebook.add(createSchemaFrame, text="Create Schema")
+notebook.add(assessSchemaFrame, text="Assess Schema")
+notebook.add(generateQueryFrame, text="Generate Query")
+
+# Pack the notebook to make it visible
+notebook.pack(fill='both', expand=True)
+
+
+"""TODO: add code for the create schema use case """
+label1 = ttk.Label(createSchemaFrame, text="ADD GUI ELEMENTS HERE")
+label1.pack(padx=10, pady=10)
+create_schema("test")
+
+"""TODO: add code for the assess schema use case """
+label2 = ttk.Label(assessSchemaFrame, text="ADD GUI ELEMENTS HERE")
+label2.pack(padx=10, pady=10)
+assess_schema("test")
+
+"""TODO: add code for the generate query use case """
+label3 = ttk.Label(generateQueryFrame, text="ADD GUI ELEMENTS HERE")
+label3.pack(padx=10, pady=10)
+generate_query("test", "test")
+
+# Start the Tkinter main loop
+root.mainloop()
--- NL_SQL_Engine.py
+++ NL_SQL_Engine.py
@@ -1,1 +1,31 @@
 #code for sql engine goes here
+
+"""
+Create schema takes in a list of strings,
+each representing an entity that schema will represent.
+The entity description must have the entity name,
+and a natural language description of all fields to be
+created. No fields not in the description will be generated
+except an ID field if model cannot identify a suitable key.
+"""
+def create_schema(entities):
+    return("NOT IMPLEMENTED") 
+
+"""
+Assess schema takes in an set of sql commands
+that would generate a schema for a database in string form.
+It returns a natural language description of 
+any erros the model detects.  
+"""
+def assess_schema(schema):
+    return("NOT IMPLEMENTED") 
+
+"""
+Generate Query takes in an set of sql commands
+that would generate a schema for a database in string form
+as well as a natural language description of a query to write.
+It returns a query in RAW SQL format.  
+"""
+#code for sql engine goes here
+def generate_query(schema, prompt):
+    return("NOT IMPLEMENTED") 
